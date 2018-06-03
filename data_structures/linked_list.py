26fe54f76c660f2d38ecee14e3bff9649ca6f82a
George Ceja <net.george.sky@gmail.com>
2018-06-03 16:49:43 -0700

    Added feature to remove node from linkedlist by value.

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -10,3 +10,19 @@ class SList:
     def insert(self, value):
         self.head = Node(value, self.head)
         self._len += 1
+
+    def remove(self, value):
+        """Removes first occurrence of node by specified value."""
+        # Check if empty, add feature to handle if user indexes null.get_value()
+        if value == self.head.get_value:
+            self.head = self.head._next
+            return True
+
+        runner = self.head
+        while runner._next._next is not None:
+            if value == runner._next.get_value():
+                runner._next = runner._next._next
+                return True
+            else:
+                runner = runner._next
+        return False
