53b8c19c0180b64ee304da472e2f4c06bb83d8a8
George Ceja <net.george.sky@gmail.com>
2018-06-05 18:34:55 -0700

    Added testcases to queue class.

---
+++ data_structures/queue/queue.py
@@ -0,0 +1,38 @@
+class Node:
+    """Simple node structure."""
+    def __init__(self, value, next=None):
+        self.val = value
+        self._next = next
+
+
+class Queue:
+    """Queue Data-structures. Used for minimal space.(Improved Queue)"""
+    def __init__(self, iter=()):
+        self.head = None
+        self.back = None
+        self._len = 0
+
+        for element in iter:
+            self.enqueue(element)
+            self._len += 1
+
+    def enqueue(self, value):
+        """APPENDS element to the back of queue."""
+        element = Node(value)
+
+        if self.head:
+            self.back._next = element
+            self.back = self.back._next
+        else:
+            self.head = element
+            self.back = element
+
+        self._len += 1
+
+    def dequeue(self):
+        """REMOVES & RETURNS element from the front of the queue object"""
+        element = self.head
+        self.head = self.head._next
+        self._len -= 1
+
+        return element
